1e1aaa2d368af391db6b9e2ec86d437fe4c72715
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-03 22:33:20 -0300

    selecciona un pokemon

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function Pokemon(nombre, color, poderDeAtaque){
+	this.nombre = nombre;
+	this.color = color;
+	this.nivelDeAmistad = 0;
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
+	this.mostrarPokemon = function(){
+		return("Hola soy: " + this.nombre + " y soy de color: " + this.color)
+	};
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+	};
+	this.atacar = function(pokemon){
+		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+	}
+}
+
+const pikashu = new Pokemon("Pikashu", "Amarillo", 50);
+const charmander = new Pokemon("Charmander", "Rojo", 20);
+const snorlax = new Pokemon("Snorlax", "Azul", 80);
+const geodude = new Pokemon("Geodude", "Gris", 40);
+
+pikashu.atacar(charmander);
+console.log(charmander.vida);
+
+function seleccionarPokemon(){
+	var poke1 = document.getElementById("seleccion").value;
+	//primero.innerHTML = pikashu.mostrarPokemon();
+	//console.log(x);
+	if(poke1 == "pikashu"){
+		primero.innerHTML = pikashu.mostrarPokemon();
+	}
+	if(poke1 == "charmander"){
+		primero.innerHTML = charmander.mostrarPokemon();
+	}
+	if(poke1 == "snorlax"){
+		primero.innerHTML = snorlax.mostrarPokemon();
+	}
+	if(poke1 == "geodude"){
+		primero.innerHTML = geodude.mostrarPokemon();
+	}
+}
