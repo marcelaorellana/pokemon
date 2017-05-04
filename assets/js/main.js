417d4e5ae910f1af840532a9f6180dedcf49177f
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-04 20:02:26 -0300

    Un poco más de estilo al html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,20 @@ function Pokemon(nombre, color, poderDeAtaque){
 	this.vida = 100;
 	this.poderDeAtaque = poderDeAtaque;
 	this.mostrarPokemon = function(){
-		return("Hola soy: " + this.nombre + " y soy de color: " + this.color)
+		return("Hola soy " + this.nombre + " y tengo " + this.poderDeAtaque + " poder de ataque");
 	};
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
 	};
 	this.atacar = function(pokemon){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
-		tercero.innerHTML = "vidas de pokemon " + pokemon.nombre + " " + pokemon.vida;
+		tercero.innerHTML = this.nombre + " Atacó a " + pokemon.nombre + " y " + pokemon.nombre + " tiene " + pokemon.vida + " de vida restante";
 	}
 }
 
-
-pikashu = new Pokemon("Pikashu", "Amarillo", 90);
-
 var poke1, poke2, pokeBatalla1, pokeBatalla2;
 
-//pikashu.atacar(charmander);
-//console.log(charmander.vida);
-
-function seleccionarPokemon(s){
+function seleccionarPokemon(){
 	poke1 = document.getElementById("seleccion").value;
 	pokeBatalla1 = new Pokemon(poke1, "Amarillo", 90);
 	primero.innerHTML = pokeBatalla1.mostrarPokemon();
@@ -34,7 +28,6 @@ function seleccionarPokemon2(){
 	poke2 = document.getElementById("seleccion2").value;
 	pokeBatalla2 = new Pokemon(poke2, "Rojo", 40);
 	segundo.innerHTML = pokeBatalla2.mostrarPokemon();
-	//console.log(pokeBatalla1);
 }
 
 function pelear(){
